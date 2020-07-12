cc1caaacfb2397a7f33156f830c293ecafd5a400
Katsuhisa Yuasa <berupon@gmail.com>
2020-07-13 02:21:10 +0900

    コード整理

--- main.cpp
+++ main.cpp
@@ -11,12 +11,13 @@
 
 #include <Windows.h>
 
+// NCHW layout
 // "SAME" zero padding 
 // stride : 1 only
 // batch size : 1 only
-void NCHW_convolution_naive_f32(
-  size_t feature_width,
-  size_t feature_height,
+void convolution_naive_f32(
+  size_t input_width,
+  size_t input_height,
   size_t kernel_width,
   size_t kernel_height,
   size_t input_channels,
@@ -33,8 +34,8 @@ void NCHW_convolution_naive_f32(
 {
   size_t padding_width = (kernel_width - 1) / 2;
   size_t padding_height = (kernel_width - 1) / 2;
-  for (size_t h=0; h<feature_height; ++h) {
-    for (size_t w=0; w<feature_width; ++w) {
+  for (size_t h=0; h<input_height; ++h) {
+    for (size_t w=0; w<input_width; ++w) {
       for (size_t m=0; m<output_channels; ++m) {
         float sum = bias[m];
         for (size_t d=0; d<input_channels; ++d) {
@@ -45,7 +46,7 @@ void NCHW_convolution_naive_f32(
               int32_t iy = (int32_t)(h + y) - padding_height;
               int32_t ix = (int32_t)(w + x) - padding_width;
               if (iy < 0 || 0 < ix
-                || iy >= feature_height || ix >= feature_width)
+                || iy >= input_height || ix >= input_width)
               {
                 continue;
               }
@@ -70,52 +71,12 @@ void NCHW_convolution_naive_f32(
   }
 }
 
-//// "SAME" zero padding 
-//// stride : 1 only
-//// batch size : 1 only
-//void NHWC_convolution_naive_f32(
-//  size_t padded_feature_width,
-//  size_t padded_feature_height,
-//  size_t kernel_width,
-//  size_t kernel_height,
-//  size_t input_channels,
-//  size_t output_channels,
-//  const float* input,
-//  const float* weight,
-//  const float* bias,
-//  float* output
-//  )
-//{
-//  size_t padding_width = kernel_width - 1;
-//  size_t padding_height = kernel_height - 1;
-//  size_t feature_width = padded_feature_width - padding_width;
-//  size_t feature_height = padded_feature_height - padding_height;
-//  for (size_t h=0; h<feature_height; ++h) {
-//    for (size_t w=0; w<feature_width; ++w) {
-//      for (size_t m=0; m<output_channels; ++m) {
-//        float sum = bias[m];
-//        for (size_t y=0; y<kernel_height; ++y) {
-//          for (size_t x=0; x<kernel_width; ++x) {
-//            for (size_t d=0; d<input_channels; ++d) {
-//              // output[h][w][m] += input[h+y][w+x][d] * weight[m][y][x][d];
-//              size_t input_index = (padded_feature_width * (h + y) + (w + x)) * input_channels + d;
-//              size_t weight_index = (m * kernel_height * kernel_width + (kernel_width * y + x)) * input_channels + d;
-//              sum += input[input_index] * weight[weight_index];
-//            }
-//          }
-//        }
-//        size_t output_index = (padded_feature_width * h + w) * output_channels + m;
-//        output[output_index] = sum;
-//      }
-//    }
-//  }
-//}
-
+// NCHW layout
 // stride : 1 only
 // batch size : 1 only
-void NCHW_im2col(
-  size_t feature_width,
-  size_t feature_height,
+void im2col(
+  size_t input_width,
+  size_t input_height,
   size_t kernel_width,
   size_t kernel_height,
   size_t input_channels,
@@ -132,14 +93,14 @@ void NCHW_im2col(
   for (size_t d=0; d<input_channels; ++d) {
     for (size_t y=0; y<kernel_height; ++y) {
       for (size_t x=0; x<kernel_width; ++x) {
-        for (size_t h=0; h<feature_height; ++h) {
-          for (size_t w=0; w<feature_width; ++w) {
-            int32_t iy = h + y;
-            int32_t ix = w + x;
+        for (size_t h=0; h<input_height; ++h) {
+          for (size_t w=0; w<input_width; ++w) {
+            int32_t iy = (int32_t)(h + y);
+            int32_t ix = (int32_t)(w + x);
             //iy -= padding_height;
             //ix -= padding_width;
             //if (iy < 0 || 0 < ix
-            //  || iy >= feature_height || ix >= feature_width)
+            //  || iy >= input_height || ix >= input_width)
             //{
             //  continue;
             //}
@@ -155,59 +116,7 @@ void NCHW_im2col(
   }
 }
 
-//void NHWC_im2col(
-//  size_t feature_width,
-//  size_t feature_height,
-//  size_t kernel_width,
-//  size_t kernel_height,
-//  size_t input_channels,
-//  const float* im,
-//  float* col
-//  )
-//{
-//  size_t padding_width = kernel_width - 1;
-//  size_t padding_height = kernel_height - 1;
-//  size_t padded_feature_width = feature_width + padding_width;
-//  size_t padded_feature_height = feature_height + padding_height;
-//
-//  size_t col_index = 0;
-//  for (size_t h=0; h<feature_height; ++h) {
-//    for (size_t w=0; w<feature_width; ++w) {
-//      for (size_t y=0; y<kernel_height; ++y) {
-//        for (size_t x=0; x<kernel_width; ++x) {
-//          for (size_t d=0; d<input_channels; ++d) {
-//            size_t im_index = ((h + y) * padded_feature_width + (w + x)) * input_channels + d;
-//            col[col_index] = im[im_index];
-//            ++col_index;
-//          }
-//        }
-//      }
-//    }
-//  }
-//}
-
-void sgemm_ver1(
-  const float* __restrict a,
-  const float* __restrict b,
-  float* __restrict c,
-  size_t M,
-  size_t N,
-  size_t K
-  )
-{
-  for (size_t m=0; m<M; ++m) {
-    for (size_t n=0; n<N; ++n) {
-      size_t cidx = N * m + n;
-      for (size_t k=0; k<K; ++k) {
-        size_t aidx = K * m + k;
-        size_t bidx = N * k + n;
-        c[cidx] += a[aidx] * b[bidx];
-      }
-    }
-  }
-}
-
-void sgemm_ver3(
+void sgemm(
   const float* __restrict a,
   const float* __restrict b,
   float* __restrict c,
@@ -224,29 +133,6 @@ void sgemm_ver3(
     }
   }
 }
-
-//void sgemm_ver4(
-//  const float* __restrict a,
-//  const float* __restrict b,
-//  float* __restrict c,
-//  size_t M,
-//  size_t N,
-//  size_t K
-//  )
-//{
-//  assert(N % 8 == 0);
-//  for (size_t i=0; i<M; ++i) {
-//    for (size_t k=0; k<K; ++k) {
-//      __m256 lik = _mm256_broadcast_ss(&a[i*M+k]);
-//      for (size_t j=0; j<N; j+=8) {
-//        __m256 o = _mm256_loadu_ps(&c[i*K+j]);
-//        __m256 r = _mm256_loadu_ps(&b[k*N+j]);
-//        o = _mm256_fmadd_ps(lik, r, o);
-//        _mm256_storeu_ps(&c[i*K+j], o);
-//      }
-//    }
-//  }
-//}
 
 LARGE_INTEGER start, stop, freq;
 size_t num_ops;
@@ -270,29 +156,29 @@ inline void measure_stop(const char* name)
 
 int main(int argc, char* argv[])
 {
-  size_t feature_width = 128;
-  size_t feature_height = 128;
+  size_t input_width = 128;
+  size_t input_height = 128;
   size_t kernel_width = 3;
   size_t kernel_height = 3;
   size_t input_channels = 64;
   size_t output_channels = 256;
 
-  num_ops = feature_height * feature_width * output_channels * input_channels * kernel_height * kernel_width * 2;
-  printf("feature_width : %zu\n", feature_width);
-  printf("feature_height : %zu\n", feature_height);
+  num_ops = input_height * input_width * output_channels * input_channels * kernel_height * kernel_width * 2;
+  printf("input_width : %zu\n", input_width);
+  printf("input_height : %zu\n", input_height);
   printf("kernel_width : %zu\n", kernel_width);
   printf("kernel_height : %zu\n", kernel_height);
   printf("input_channels : %zu\n", input_channels);
   printf("output_channels : %zu\n", output_channels);
   printf("giga floating point operations : %.1f\n", num_ops/(double)(1000*1000*1000));
 
-  size_t padded_feature_width = feature_width + kernel_width - 1;
-  size_t padded_feature_height = feature_height + kernel_height - 1;
-  size_t input_bytes = padded_feature_height * padded_feature_width * input_channels * sizeof(float);
-  size_t col_bytes = (kernel_height * kernel_width * input_channels) * (padded_feature_height * padded_feature_width) * sizeof(float);
+  size_t padded_input_width = input_width + kernel_width - 1;
+  size_t padded_input_height = input_height + kernel_height - 1;
+  size_t input_bytes = padded_input_height * padded_input_width * input_channels * sizeof(float);
+  size_t col_bytes = (kernel_height * kernel_width * input_channels) * (padded_input_height * padded_input_width) * sizeof(float);
   size_t weight_bytes = output_channels * (kernel_height * kernel_width * input_channels) * sizeof(float);
   size_t bias_bytes = output_channels * sizeof(float);
-  size_t output_bytes = padded_feature_height * padded_feature_width * output_channels * sizeof(float);
+  size_t output_bytes = padded_input_height * padded_input_width * output_channels * sizeof(float);
   float* input = (float*)malloc(input_bytes);
   float* col = (float*)malloc(col_bytes);
   float* weight = (float*)malloc(weight_bytes);
@@ -312,10 +198,10 @@ int main(int argc, char* argv[])
   std::uniform_real_distribution<float> dist(0.0f, 10.0f);
   mt.seed();
   for (size_t i=0; i<input_channels; ++i) {
-    for (size_t j=0; j<feature_height; ++j) {
-      for (size_t k=0; k<feature_width; ++k) {
-        size_t input_index = i * padded_feature_height * padded_feature_width
-                             + (j + 1) * padded_feature_width + (k + 1);
+    for (size_t j=0; j<input_height; ++j) {
+      for (size_t k=0; k<input_width; ++k) {
+        size_t input_index = i * padded_input_height * padded_input_width
+                             + (j + 1) * padded_input_width + (k + 1);
         input[input_index] = dist(mt);
       }
     }
@@ -327,91 +213,53 @@ int main(int argc, char* argv[])
   QueryPerformanceFrequency(&freq);
 
   measure_start();
-  NCHW_convolution_naive_f32(
-    feature_width,
-    feature_height,
+  convolution_naive_f32(
+    input_width,
+    input_height,
     kernel_width,
     kernel_height,
     input_channels,
     output_channels,
-    padded_feature_width,
-    padded_feature_height * padded_feature_width,
-    padded_feature_width,
-    padded_feature_height * padded_feature_width,
+    padded_input_width,
+    padded_input_height * padded_input_width,
+    padded_input_width,
+    padded_input_height * padded_input_width,
     input,
     weight,
     bias,
-    output0 + padded_feature_width + 1
+    output0 + padded_input_width + 1
   );
-  measure_stop("NCHW_convolution_naive_f32");
+  measure_stop("convolution_naive_f32");
 
-  //measure_start();
-  //NHWC_convolution_naive_f32(
-  //  padded_feature_width,
-  //  padded_feature_height,
-  //  kernel_width,
-  //  kernel_height,
-  //  input_channels,
-  //  output_channels,
-  //  input,
-  //  weight,
-  //  bias,
-  //  output0 + padded_feature_width + 1
-  //);
-  //measure_stop("NHWC_convolution_naive_f32");
-  
   should_print_flops = false;
   measure_start();
-  NCHW_im2col(
-    feature_width,
-    feature_height,
+  im2col(
+    input_width,
+    input_height,
     kernel_width,
     kernel_height,
     input_channels,
-    padded_feature_width,
-    padded_feature_height * padded_feature_width,
-    padded_feature_width,
-    padded_feature_height * padded_feature_width,
+    padded_input_width,
+    padded_input_height * padded_input_width,
+    padded_input_width,
+    padded_input_height * padded_input_width,
     input,
-    col + padded_feature_width + 1
+    col + padded_input_width + 1
   );
-  measure_stop("NCHW_im2col");
-
-  //measure_start();
-  //NHWC_im2col(
-  //  feature_width,
-  //  feature_height,
-  //  kernel_width,
-  //  kernel_height,
-  //  input_channels,
-  //  input,
-  //  col
-  //);
-  //measure_stop("NHWC_im2col");
+  measure_stop("im2col");
 
   should_print_flops = true;
 
   size_t acols = (input_channels * kernel_height * kernel_width);
   size_t arows = output_channels;
-  size_t bcols = (padded_feature_height * padded_feature_width);
+  size_t bcols = (padded_input_height * padded_input_width);
 
   size_t m = arows;
   size_t n = bcols;
   size_t k = acols;
 
-  //measure_start();
-  //sgemm_ver1(
-  //  weight,
-  //  col,
-  //  output1,
-  //  m,
-  //  n,
-  //  k
-  //);
-  //measure_stop("sgemm_ver1");
-
   measure_start();
-  sgemm_ver3(
+  sgemm(
     weight,
     col,
     output1,
@@ -420,17 +268,6 @@ int main(int argc, char* argv[])
     k
   );
   measure_stop("sgemm_ver3");
-
-  //measure_start();
-  //sgemm_ver4(
-  //  weight,
-  //  col,
-  //  output2,
-  //  m,
-  //  n,
-  //  k
-  //);
-  //measure_stop("sgemm_ver4");
 
   measure_start();
   avx256_noncblas_sgemm(
